240a3937a4bf37317e820aa342a32fa2a2c9946e
Aiko <aikocute@icloud.com>
2022-12-23 09:56:00 +0700

    Update DEB Sub

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+read -p "Enter iOS version: " ios
+
+# check file palera1n-v1.4.1.deb exists
+if [ ! -f palera1n-v1.4.1.deb ]; then
+	echo "File palera1n-v1.4.1.deb not found!"
+	exit 1
+fi
+
+sudo apt-get update
+
+sudo add-apt-repository universe
+
+sudo apt install libimobiledevice-utils libusbmuxd-tools git curl python3-pip -y
+
+sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
+
+sudo dpkg -i palera1n-v1.4.1.deb
+
+sudo systemctl stop usbmuxd
+
+sudo usbmuxd -f -p
+
+sudo ./palera1n.sh --tweaks $ios --semi-tethered
+
+
